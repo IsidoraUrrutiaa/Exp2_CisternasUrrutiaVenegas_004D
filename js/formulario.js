cd80fe59d9abbb86c72c5a37cdda0deffe4de72b
IsidoraUrrutiaa <isid.urrutia@duocuc.cl>
2021-05-11 20:12:52 -0400

    Incorporando Registro

---
+++ js/formulario.js
@@ -0,0 +1,46 @@
+
+    $(function() 
+    {
+      $("#mi-formulario").validate({
+           rules: {
+                  email: {
+                      required: true,
+                      email: true
+                  },
+                  password: "required",
+                  fono: "required",
+                  nombre: "required",
+                  password2: {
+                      required: true,
+                      equalTo: "#password"
+                  }   
+                  
+              }, //rules
+          messages: {
+              email: {
+                  required: 'Ingresa tu correo electrónico',
+                  email: 'Formato de correo no válido'
+              },
+              password: {
+                  required: 'Ingresa una contraseña',
+                  minlength: 'Caracteres insuficientes'
+              },
+              fono:{
+                  required: 'Ingrese un número de celular',
+                  minlength: 'Cantidad de digitos insuficiente'
+              },
+              nombre:{
+                  required: 'Ingrese Nombre',
+                  min: 'Fecha no corresponde'
+              },
+              password2: {
+                  required: 'Reingresa la contraseña',
+                  equalTo: 'Las contraseñas ingresadas no coinciden',
+                  minlength: 'Caracteres insuficientes'
+
+
+              }
+          }//messages
+      }); //$('#mi-formulario').validate
+  }); //function 
+
